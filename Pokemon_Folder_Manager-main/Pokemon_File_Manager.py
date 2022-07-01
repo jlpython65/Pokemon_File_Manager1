fb733b63864efbba84b4840752bbef19bb8c4688
unknown <jasonpythonacc@gmail.com>
2022-07-01 15:19:03 -0400

    test

--- Pokemon_Folder_Manager-main/Pokemon_File_Manager.py
+++ Pokemon_Folder_Manager-main/Pokemon_File_Manager.py
@@ -38,14 +38,14 @@ def rename_pokemon(dir_list):
         for x in range(int(len(dir_list))):
             PATH=dir_list[x]
             PATH_FOR_REG = dir_list[x].replace("\\","\\\\")
-            print("Looking through path: " + f'{PATH}')
             for image_filename in glob.iglob(fr'{PATH}'+fr"/{NUMBER}.png"):
                 print(image_filename)
                 os.rename(image_filename,fr'{PATH}'+"\\"+f"{NAME}"+".png")
-
-                if 
-                except:
-                    os.rename(image_filename,fr'{PATH}'+"\\"+f"{NUMBER}".replace(f'{NUMBER}',f'{NAME}'))
+            for image_filename in glob.iglob(fr'{PATH}'+fr"/*.png"):
+                if re.search(f"{NUMBER}_",image_filename):
+                    print("_")
+                    os.rename(image_filename,image_filename.replace(f'{NUMBER}',f'{NAME}'))
+#There are a lot of changes in the formatting of these names which makes using regex to search for them tedious. So are there any solutions to that? I was thinking making more if statements. As you see in the folders, there are pokemon with "m" and "f" or "type-" in the filename. I didn't account for those in this iteration.
 
 dir_list= []
 
